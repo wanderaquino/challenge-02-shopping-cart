3ce718555c2262a2e8d44bb628583f36beb75862
Wander Aquino da Cruz <wander.aquino@gmail.com>
2021-07-16 23:00:30 -0300

    Implementing count of cart itens.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,8 +7,17 @@ import { Container, Cart } from './styles';
 import { useCart } from '../../hooks/useCart';
 
 const Header = (): JSX.Element => {
-  // const { cart } = useCart();
-  // const cartSize = // TODO;
+  const { cart } = useCart();
+
+  const cartSize = Object.keys(cart.reduce((acc,curr) => {
+    if( acc[`${curr.id}`]) {
+      acc[`${curr.id}`] += 1; 
+      return acc;
+    } else {
+      acc[`${curr.id}`] = 1;
+      return acc;
+    }
+  }, {} as any)).length;
 
   return (
     <Container>
@@ -20,7 +29,7 @@ const Header = (): JSX.Element => {
         <div>
           <strong>Meu carrinho</strong>
           <span data-testid="cart-size">
-            {/* {cartSize === 1 ? `${cartSize} item` : `${cartSize} itens`} */}
+            {cartSize === 1 ? `${cartSize} item` : `${cartSize} itens`}
           </span>
         </div>
         <MdShoppingBasket size={36} color="#FFF" />
